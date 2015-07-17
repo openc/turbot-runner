cdc4d3efd4c30234d99afc5962845f82cc4599c1
Peter Inglesby <peter.inglesby@gmail.com>
2015-07-17 16:07:32 +0100

    Only allow publishing from master branch

--- publish.sh
+++ publish.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ $branch != "master" ]]; then
+	echo "Must be run from master!"
+	exit 1
+fi
+
 gem build turbot-runner.gemspec
 gem push $(ls *gem|tail -1)
 
